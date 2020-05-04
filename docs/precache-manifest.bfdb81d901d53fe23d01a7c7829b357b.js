b36fcd296ef61801523f8a2578c2dd57eff454e8
amazingshellyyy <amazingshellyyy@gmail.com>
2020-05-04 15:29:23 -0700

    deploy

---
+++ docs/precache-manifest.bfdb81d901d53fe23d01a7c7829b357b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78e1435d74367c2e99ab403d0ceee70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0842778440a6689cfda",
+    "url": "/static/js/2.c8c61f79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c8c61f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d09f452c6910c2610c",
+    "url": "/static/js/main.e2f63bf5.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
